6b0759ca539763b2237927506ec1086dc6a2f223
Andabe <142181128+Andabe-Net@users.noreply.github.com>
2024-06-15 10:31:44 +0100

    Update script.sh

--- setting-up-jenkins-on-azure-vm/script.sh
+++ setting-up-jenkins-on-azure-vm/script.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/bash
 #create resource group and deploy a virtual machine in it using the arm-template file
+echo 'creating resource group....'
 az group create --name rg-jenkins-on-azure-vm-eastus --location eastus
+echo 'deploying template...'
 az deployment group create --name rollout1 -g rg-jenkins-on-azure-vm-eastus --template-file arm-template.json -o yaml
